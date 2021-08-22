6b987602b323be16a961a9d2e45bfe6310224310
diegocancela <88489918+diegocancela@users.noreply.github.com>
2021-08-22 20:58:40 -0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -2,5 +2,14 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
+    fetch(PRODUCTS_URL) //Realizamos el fetch que devolverá una promesa
+        .then(respuesta => respuesta.json()) //Obtenemos una promesa que trataremos como json
+        .then(datos => { //obtenemos una nueva promesa, pero los datos ya están como json.
+            const tbody = document.getElementById("productInfo");
+            datos.forEach(element => {
+                tbody.innerHTML += `<tr><td>${element.name}</td><td>${element.description}</td><td>${element.cost}</td></tr>`;
+            });
+        })
+        .catch(error => alert("Hubo un error: " + error));
 
 });
