4ddb653b08b775b6d484aaeca737912189630769
diegocancela <88489918+diegocancela@users.noreply.github.com>
2021-08-22 19:43:16 -0300

    update js

--- js/init.js
+++ js/init.js
@@ -44,4 +44,8 @@ var getJSONData = function(url){
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
+  var usuario = localStorage.getItem("user");
+  var pass = localStorage.getItem("password");
+  if (!usuario || !pass) {
+    location.href="login.html";
 });
--- js/login.js
+++ js/login.js
@@ -2,5 +2,13 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
-
+    if (userName.trim() === "" || password.trim() === "") { //Chequea que el dato recibido no esté vacío. 
+        //El método trim elimina los espacios en blanco al inicio y al final del mismo.
+        alert("Error: usuario o contraseña vacio");
+    } else {
+        localStorage.setItem("user", userName.trim()); //setItem almacena el dato en la posición "usuario"
+        localStorage.setItem("password", password.trim()); // Almaceno la contraseña
+        sessionStorage.setItem("user", userName.trim());
+        location.href = "index.html";
+        //getItem obtiene el dato almacenado en la posición "usuario"
 });
