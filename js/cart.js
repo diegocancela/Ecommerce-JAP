ed4c01af58c47878c72a2794b3b9de39332f4aea
diegocancela <88489918+diegocancela@users.noreply.github.com>
2021-11-14 23:16:14 -0300

    entrega 6 sin grupal y entrega 7 completa

--- js/cart.js
+++ js/cart.js
@@ -28,7 +28,7 @@ document.addEventListener("DOMContentLoaded", function (e) {
                         <div class="col">
                             <div class="d-flex w-100 justify-content-between">
                                 <h4 class="mb-1">`+ c.name + `</h4>
-                                <input type="number" onchange="calculatePrice()" id="quantity" name="quantity" class="text-muted"  min="1" value="`+ c.count + `"> Unidades</input>
+                                <input type="number" onchange="calculatePrice()" id="quantity" name="quantity" class="text-muted"  min="0" value="`+ c.count + `"> Unidades</input>
                                 </div>
                                 <p class="mb-1">` + c.unitCost + `</p>
                                 <div><small class="text-muted">` + c.currency + `</small></div>
@@ -38,7 +38,7 @@ document.addEventListener("DOMContentLoaded", function (e) {
                 <p></p>
                 `
             }
-            total = subtotal + 200;
+            total = subtotal;
             htmlContentToAppend += `<p></p>`
             document.getElementById("cart-list-container").innerHTML = htmlContentToAppend;
             document.getElementById("subTotalValue").innerHTML = subtotal;
@@ -63,9 +63,39 @@ function calculatePrice() {
             subtotal += c.unitCost * values[i].value;
         }*/
     }
-    total = subtotal + 200;
+    let envio = 0;
+    if (document.getElementById('Premium').checked) {
+        envio = subtotal * 0.15;
+    }
+    else if (document.getElementById('Express').checked) {
+        envio = subtotal * 0.07;
+    }
+    else if (document.getElementById('Standard').checked) {
+        envio = subtotal * 0.05;
+    }
+    total = subtotal + envio;
     document.getElementById("subTotalValue").innerHTML = subtotal;
     document.getElementById("totalValue").innerHTML = total;
+}
+
+function validarCompra() {
+    let listElements=document.getElementsByName("quantity");
+    let cantidadProducto=true;
+    for (let i = 0; i < listElements.length; i++){
+        if (listElements[i].value==0){
+            cantidadProducto=false;
+            alert('No se ha determinado la cantidad de uno de los productos')
+        }
+    }
+    if (!(document.getElementById('creditCard').checked || document.getElementById('transfer').checked)) {
+        alert('No se ha seleccionado metodo de pago');
+    }
+    else if (!(document.getElementById('Premium').checked || document.getElementById('Express').checked || document.getElementById('Standard').checked)) {
+        alert('No se ha seleccionado metodo de envio');
+    } 
+    else if (cantidadProducto){
+        alert('Compra exitosa');
+    }
 }
 
 function getValueInPesos(subtotal, count, c) {
--- js/my-profile.js
+++ js/my-profile.js
@@ -2,5 +2,34 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
-
+    let jsonString = localStorage.getItem("userData");
+    let json = JSON.parse(jsonString);
+    if (json.name != '' || json.surname != '' || json.email != '' || json.phone != '' || json.age != '') {
+        document.getElementById("profileName").value = json.name;
+        document.getElementById("profileSurname").value = json.surname;
+        document.getElementById("profileAge").value = json.age;
+        document.getElementById("profileEmail").value = json.email;
+        document.getElementById("profilePhone").value = json.phone;
+    }
 });
+
+
+function cargarDatos() {
+    let _name = document.getElementById("profileName").value;
+    let _surname = document.getElementById("profileSurname").value;
+    let _age = document.getElementById("profileAge").value;
+    let _email = document.getElementById("profileEmail").value;
+    let _phone = document.getElementById("profilePhone").value;
+    _name = _name.trim();
+    _surname = _surname.trim();
+    _email = _email.trim();
+    _phone = _phone.trim();
+    if (_name == '' || _surname == '' || _email == '' || _phone == '' || _age == '') {
+        alert('Se ha ingresado un dato vacio')
+    } else {
+        let data = { name: _name, surname: _surname, age: _age, email: _email, phone: _phone }
+        let json = JSON.stringify(data);
+        localStorage.setItem("userData", json);
+        alert('Datos guardados correctamente')
+    }
+}
